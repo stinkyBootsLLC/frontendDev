c989ed7687a6f2df621f1a7585c3d1cf369f7158
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2019-07-07 19:14:24 -0400

    visual style added

--- pwned/scripts/siteFunc.js
+++ pwned/scripts/siteFunc.js
@@ -5,65 +5,53 @@
 // password hasn't already been hacked
 
     function createTheAPIcall(){
-       let userInput = document.getElementById("passField").value;
-        // console.log("password =  " + userInput);
-        let shaHex = sha1(userInput);
-        let shaKey = shaHex.substring(0, 5); // first four to send
-        let shaMain = shaHex.substring(5, 40);// the part i want to search for in the results
-        let shaMain2 = shaMain.toUpperCase();
-        // console.log("full HEX =  " + shaHex);
-        // console.log("Key =  " + shaKey);
-         console.log("part to search = " + shaMain);
-        let url = "https://api.pwnedpasswords.com/range/" + shaKey; // url that returns the list
-
-
-
-
-       console.log("url = " + url);
-       callTheAPI(url, shaMain2);
-
-
-
-        
-
+      let userInput = document.getElementById("passField").value;
+      let shaHex = sha1(userInput);
+      let shaKey = shaHex.substring(0, 5); // first four to send
+      let shaMain = shaHex.substring(5, 40);// the part i want to search for in the results
+      let shaMain2 = shaMain.toUpperCase();
+      // console.log("full HEX =  " + shaHex);
+      // console.log("Key =  " + shaKey);
+      // console.log("part to search = " + shaMain);
+      let url = "https://api.pwnedpasswords.com/range/" + shaKey; // url that returns the list
+      //console.log("url = " + url);
+      callTheAPI(url, shaMain2);
     }
 
     function callTheAPI(address, find){
-      console.log("address = " + address);
-
-
-
-
-
-   
-
-
-
-
+      // console.log("address = " + address);
       // make a request 
       let apiRequest = new XMLHttpRequest();
       apiRequest.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           // store the returned in a string
           let responseString = this.responseText;
-          console.log(responseString);
+          // console.log(responseString);
 
           let findme = responseString.indexOf(find);// number at start position
           if (findme > 0){
-            document.getElementById("found2").innerHTML = "Password is compromised";
+            //.setAttribute("class", "democlass"); 
+            // .style.backgroundColor = "red";
+            let badPassResult = document.getElementById("display");
+            badPassResult.style.color = "red";
+            badPassResult.innerHTML = "this password is compromised!";
+            //document.getElementById("found2").innerHTML = "Password is compromised";
 
           } else if (findme < 0){
-            document.getElementById("found2").innerHTML = "wow you found a clean one!";
+            let goodPassResult = document.getElementById("display");
+            goodPassResult.style.color = "green";
+            goodPassResult.innerHTML = "wow you found a clean one!";
+            //document.getElementById("found2").innerHTML = "wow you found a clean one!";
 
           }
           
-          document.getElementById("found").innerHTML = findme;
+          //document.getElementById("found").innerHTML = findme;
           
 
 
 
           
-        }
+        }// end if (find me)
       };
       apiRequest.open("GET", address, true);
       apiRequest.send();
