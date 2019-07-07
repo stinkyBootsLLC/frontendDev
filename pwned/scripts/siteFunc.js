0d90edb5e6336d96d5bb7e69ad1936aadf723f16
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2019-07-07 17:16:40 -0400

    algo working
    needs refractoring

--- pwned/scripts/siteFunc.js
+++ pwned/scripts/siteFunc.js
@@ -1,13 +1,16 @@
+// https://haveibeenpwned.com/API/v2
+// https://www.youtube.com/watch?v=hhUb5iknVJs
+// Have You Been Pwned? - Computerphile
+// Would you type your password into a random box on the internet? Dr Mike Pound on ensuring your 
+// password hasn't already been hacked
 
-  
-
- 
     function createTheAPIcall(){
        let userInput = document.getElementById("passField").value;
         // console.log("password =  " + userInput);
         let shaHex = sha1(userInput);
         let shaKey = shaHex.substring(0, 5); // first four to send
         let shaMain = shaHex.substring(5, 40);// the part i want to search for in the results
+        let shaMain2 = shaMain.toUpperCase();
         // console.log("full HEX =  " + shaHex);
         // console.log("Key =  " + shaKey);
          console.log("part to search = " + shaMain);
@@ -16,72 +19,62 @@
 
 
 
-           // console.log(address);
+       console.log("url = " + url);
+       callTheAPI(url, shaMain2);
 
-        // make a request 
-        let apiRequest = new XMLHttpRequest();
-        apiRequest.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            // store the returned in a string
-            let responseString = this.responseText;
 
-            let test = shaMain;
-            console.log(responseString);
-            console.log(typeof responseString);
-            //createArray(responseString)
-            var x = responseString.search(test);
-      
-                 console.log("----"+x);
-          }
-        };
-        apiRequest.open("GET", url, true);
-        apiRequest.send();
-
-        //callTheAPI(url);
 
         
 
     }
 
-    // function callTheAPI(address){
-    //     // // console.log(address);
+    function callTheAPI(address, find){
+      console.log("address = " + address);
 
-    //     // // make a request 
-    //     // let apiRequest = new XMLHttpRequest();
-    //     // apiRequest.onreadystatechange = function() {
-    //     //   if (this.readyState == 4 && this.status == 200) {
-    //     //     // store the returned in a string
-    //     //     let responseString = this.responseText;
-    //     //     // console.log(responseString);
-    //     //     // console.log(typeof responseString);
-    //     //     createArray(responseString)
-    //     //   }
-    //     // };
-    //     // apiRequest.open("GET", address, true);
-    //     // apiRequest.send();
+
+
+
+
+   
+
+
+
+
+      // make a request 
+      let apiRequest = new XMLHttpRequest();
+      apiRequest.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          // store the returned in a string
+          let responseString = this.responseText;
+          console.log(responseString);
+
+          let findme = responseString.indexOf(find);// number at start position
+          if (findme > 0){
+            document.getElementById("found2").innerHTML = "Password is compromised";
+
+          } else if (findme < 0){
+            document.getElementById("found2").innerHTML = "wow you found a clean one!";
+
+          }
+          
+          document.getElementById("found").innerHTML = findme;
+          
+
+
+
+          
+        }
+      };
+      apiRequest.open("GET", address, true);
+      apiRequest.send();
+
+      
 
         
         
 
-    // }
+    }
 
-    // function createArray(aString, findMainHex){
-        //var res = aString.split(":");
-        // var x = aString.search(findMainHex);
-      
-        // console.log("----"+res);
-        // console.log(typeof res);
-        //console.log(res[6]);
-/*
-        this will not work 
-        the next index starts after the comma
 
-        000DE94C4C9A9C72B4D02C8BBCB5F3627A5,1
-        002EDB2852C132D9EEB2BA012862AA243DC,6
-*/
-
-      
-
-    // }
 
  
