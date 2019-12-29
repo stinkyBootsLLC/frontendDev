8a2202ed869274810234ec2a57907dd8ad79cc04
stinkyBootsLLC <stinkyBootsLLC@gmail.com>
2019-12-29 10:04:42 -0500

    cleaning

--- recipes/process.php
+++ recipes/process.php
@@ -7,14 +7,12 @@
         $values[] = $value;
     }// end foreach
     $post_results = array_combine($fields, $values);
-///// get all the post data
-
+///// end get all the post data
 
     $items = array();
     $ingredients = array();
     $steps = array();
     $directions = array();
-
 
     foreach($post_results as $field => $value) {
 
@@ -34,34 +32,12 @@
         if (strstr($field, 'step_')){
             $steps["Step"] = $value;
             array_push($directions,$steps);
-
         }
+    }// end foreach($post_results as $field => $value) 
 
-    
-
-
-
-
-
-
-   
-    }
-
-
-
-    // print_r($ingredients);
-    // print_r($directions);
-
-
-
-
-
-
-
-
-
-    if(file_exists('recipes.json')){  
-        $current_data = file_get_contents('recipes.json');  
+    if(file_exists('assets/db/recipes.json')){  
+        // append to end of json file
+        $current_data = file_get_contents('assets/db/recipes.json');  
         $array_data = json_decode($current_data, true);  
         $extra = array(  
             'id'             =>     $_POST['id'],  
@@ -69,24 +45,14 @@
             'description'    =>     $_POST["description"],
             'ingredients'    =>     $ingredients,
             'directions'     =>     $directions
-    );  
+        );  
 
         $array_data[] = $extra;  
         $final_data = json_encode($array_data);  
-        if(file_put_contents('recipes.json', $final_data)){  
+        if(file_put_contents('assets/db/recipes.json', $final_data)){  
             echo "<label class='text-success'>File Appended Success fully</p>";  
         }  
     } else {  
         echo "JSON File not exits";  
-    }  
-
-
-
-
-
-
-
-
-
-
+    } // end if(file_exists()) 
 ?>
