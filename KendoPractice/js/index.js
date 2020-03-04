0370ae790dcd54d3c3b294ba1a57f2720d46788a
stinkyBootsLLC <stinkybootsllc@gmail.com>
2020-03-04 11:13:40 -0500

    clean and documented

--- KendoPractice/js/index.js
+++ KendoPractice/js/index.js
@@ -1,6 +1,10 @@
-
-
-
+/**
+ * Main Script to display two Kendon components. (grid, chart).
+ * Everything is wrapped in functions to avoid global variables.
+ * Global variables  may lead to a maintenance issue. 
+ * @author Eduardo Estrada
+ * @date 3-4-2020
+ */
 /**
  * Returns array of headers
  * @param {array} returnedTarget 
@@ -10,32 +14,28 @@ function getHeaders(returnedTarget){
     let headers = [];
     for (var key in returnedTarget[1]) {
         if (returnedTarget[1].hasOwnProperty(key)) {
-            // console.log(key + " -> " + returnedTarget[1][key]);
             headers.push(key);
         }
     }// end for
     return headers
-
 }// end getHeaders()
-
 
 /**
  * Displays Kendo Grid to div element.
  * @param {JSON Object} extdata 
  */
 function displayTable(extdata){
+    /** Organize the data */
     // declare local variables
-    let data = [];
-    
+    let dataArray = [];
     // copy only the required data to empty data array
-    const returnedTarget = Object.assign(data, extdata["Major Sector Productivity and Costs"]);
+    const returnedTarget = Object.assign(dataArray, extdata["Major Sector Productivity and Costs"]);
     // get headers
     let headers = getHeaders(returnedTarget);
-
     // get the title
     let titleArr = Object.keys(extdata);
     let titleString = titleArr[0].toString();
-
+    /** End - Organize the data */
     // now using the kendo lib
     $("#grid").kendoGrid({
         toolbar: ["excel"],
@@ -45,74 +45,52 @@ function displayTable(extdata){
             filterable: true
         },
         sortable: true,
-        // pageable: true,
-        // groupable: true,
         filterable: true,
-        columnMenu: true, //this one
-        // reorderable: true,
-        // resizable: true,
+        columnMenu: true, 
         columns: [
             {
-                title: titleString + " (output per hour)",// "title",
-                columns: headers // working array 
-                // [
-                //     { field: headers[0], filterable: true},
-                //     { field: headers[1], filterable: true},
-                //     { field: headers[2], filterable: true},
-                //     { field: headers[3], filterable: true }
-                   
-                // ]
+                title: titleString + " (output per hour)",
+                /**
+                 * Example of what can be in the array
+                 * Objects of properties
+                 * [ { field: headers[0], filterable: true} ] 
+                 */
+                columns: headers 
             }
         ],
         editable: false,
         // this must be an array 
-        dataSource:  data, // fields have to match
+        dataSource:  dataArray, // fields have to match
         height: 400
     });
 }// end displayTable()
-
+/**
+ * Displays Kendo line Chart to div element.
+ * @param {JSON Object} extdata 
+ */
 function createChart(extdata) {
-
+    /** Organize the data */
     // declare local variables
-    let data = [];
+    let dataArray = [];
     // copy only the required data to empty data array
-    const returnedTarget = Object.assign(data, extdata["Major Sector Productivity and Costs"]);
+    const returnedTarget =  Object.assign(dataArray, extdata["Major Sector Productivity and Costs"]);
     // get the title
     let titleArr = Object.keys(extdata);
     let titleString = titleArr[0].toString();
     // get series names
     let seriesNames = getHeaders(returnedTarget);
-    console.log(returnedTarget);
-
-    // var nameArray = returnedTarget.map(function (el) { return parseFloat(el.Qtr1) });
-    
-
+    // get series values
     let series1 = returnedTarget.map(function (el) { return parseFloat(el.Qtr1) });
-    // console.log(series1);
     let series2 = returnedTarget.map(function (el) { return parseFloat(el.Qtr2) });
-    // console.log(series2);
     let series3 = returnedTarget.map(function (el) { return parseFloat(el.Qtr3) });
-    // console.log(series3);
     let series4 = returnedTarget.map(function (el) { return parseFloat(el.Qtr4) });
-    // console.log(series4);
-
-    let xLabels = returnedTarget.map(function (el) { return parseInt(el.Year) });
-    console.log(xLabels);
-
-   
-
-
-
-
-     
-
-
-
-
+    // Get only the years for x axis labels
+    let xAxisLabels = dataArray.map(function (el) { return parseInt(el.Year) });
+    /** End - Organize the data */
+    // Kendo Library
     $("#chart").kendoChart({
         title: {
             text: titleString + " (output per hour)"
-
         },
         legend: {
             position: "bottom"
@@ -127,42 +105,19 @@ function createChart(extdata) {
         },
         series: [{
             name: seriesNames[1], // string
-
-           
             color: "#236AB9",
-                
-           
-
-
-
-
             data: series1 // needs to be an array
         },{
             name: seriesNames[2], // string
-
             color: "#FC7307",
-
-
-
-
             data: series2 // needs to be an array
         },{
             name: seriesNames[3], // string
-
             color: "#B85B14",
-
-
-
-
-
             data: series3 // needs to be an array
         },{
             name: seriesNames[4], // string
-
             color: "#341C09",
-           
-
-
             data: series4 // needs to be an array
         }],
         valueAxis: {
@@ -175,7 +130,7 @@ function createChart(extdata) {
             axisCrossingValue: -10
         },
         categoryAxis: {
-            categories: xLabels,//[2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011],
+            categories: xAxisLabels,
             majorGridLines: {
                 visible: false
             },
@@ -189,9 +144,17 @@ function createChart(extdata) {
             template: "#= series.name #: #= value #"
         }
     });
-}
-
+}// end createChart()
+/**
+ * Gets the data source and calls a function.
+ * JQuery ajax method, Performs an asynchronous HTTP (Ajax) request.
+ * @param {String} url 
+ * @param {String} component 
+ */
 function getData(url, component){
+    /**
+     * jQuery.ajax( url [, settings ] )
+     */
     $.ajax({
         url: url, 
         contentType: "application/json",
@@ -199,7 +162,6 @@ function getData(url, component){
         success: function(result){
             // assync methods CANNOT return 
             // call the function
-            
             switch(component) {
                 case "grid":
                     displayTable(result);
@@ -209,19 +171,16 @@ function getData(url, component){
                   break;
                 default:
                   // code block
-              }
-
-
+              }// end switch
         },
-        error: function(e){
-            alert("Unable to fetch external data "+ e.message + " - error");
+        error: function(error){
+            alert("Unable to fetch external JSON data "+ error.message + " " + component);
         }
     });
 }// end getData()
-
-
-
-
+/**
+ * Start the sequence
+ */
 $( document ).ready(function() {
     getData("http://localhost/KendoPractice/dataSource/qryLaborProd.json","grid");
     getData("http://localhost/KendoPractice/dataSource/qryLaborProd.json","line-chart");
