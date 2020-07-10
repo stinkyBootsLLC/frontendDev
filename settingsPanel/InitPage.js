88c419d78294dd572ea1ae8151ef15c39604229f
stinkyBootsLLC <stinkybootsllc@gmail.com>
2020-07-10 11:51:52 -0400

    added missing files

---
+++ settingsPanel/InitPage.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    'sap/ui/Device'
+    ], function(Device) {
+    "use strict";
+        // "InitPage.js", <= this goes in manifest.json file dependency
+    return {
+
+        initPageSettings : function(oView) {
+            console.log("test");
+        }
+    };
+});
