ac98eab176b7e13a12d2ac8366866a968a67e4d7
stinkyBootsLLC <stinkybootsllc@gmail.com>
2020-07-11 10:33:37 -0400

    new layout
    started to make week and month display as a user change

--- covidChart/index.js
+++ covidChart/index.js
@@ -13,82 +13,82 @@ let chartInfo = {
     total_deaths: 0,
     total_cases: 0,
 };
+let stats = [];
+
+function refresh() {
+    let chart = $("#cases-chart").data("kendoChart"),
+        categoryAxis = chart.options.categoryAxis,
+        baseUnitInputs = $("input:radio[name=baseUnit]");
+
+    categoryAxis.baseUnit = baseUnitInputs.filter(":checked").val();
+
+    chart.refresh();
+}// end refresh()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
- * Creates Arc Gauges
+ * Adds Value to html div
  * @param {string} sId - html element I.D
  * @param {Number} nValue - value of the gauge
  */
-function createGauge(sId, nValue) {
-    $("#" + sId).kendoArcGauge({
-        color: "#67a9f5",
-        value: nValue,
-        centerTemplate: '#: value #%'
-       
-    });
+function displayTotalStats(sId, nValue) {
+    $( "#" + sId ).html( nValue + "%" );
 }
 
 /**
  * Creates a Kendo UI Chart.  
  * Added parameters to make re-usuable
- * @param {string} sID - html element I.D
- * @param {string} sMainTitle - Title of the Chart
- * @param {string} sSeriesType - Chart Type
- * @param {string} sSeriesName - Chart Series name
- * @param {string} sSeriesColor - Chart Series color
- * @param {array} aSeriesData - Chart Series data
- * @param {array} aCategories - Chart categories (x axis)
+ * @param {string} id - selector
+ * @param {object} oData - data
+ * @param {string} sField - property of oData
+ * @param {string} sBaseUnits - days weeks months years
+ * @param {string} sSeriesColor - css color
+ * @param {string} sMainTitle - chart title
+ * @param {string} sSeriesType - determines the chart type
  */
-function createChart(sID, sMainTitle, sSeriesType, sSeriesName, sSeriesColor, aSeriesData, aCategories) {
- 
-    $("#" + sID).kendoChart({
+function createChart(id, oData, sField, sBaseUnits, sSeriesColor, sMainTitle, sSeriesType){
+    $("#" + id).kendoChart({
+        dataSource: {
+            data: oData
+        },
         title: {
             text: sMainTitle
         },
         legend: {
             position: "bottom"
         },
-        chartArea: {
-            background: ""
-        },
-        seriesDefaults: {
-           // type: "area",
-            style: "smooth"
-        },
         seriesColors: [sSeriesColor],
         series: [{
             type: sSeriesType,
-            name: sSeriesName,
-            data: aSeriesData
-        },],
-        valueAxis: {
-            labels: {
-                format: "N0"
-            },
-            line: {
-                visible: false
-            },
-            axisCrossingValue: -10
-        },
+           // aggregate: "avg",
+            field: sField,
+            categoryField: "date"
+        }],
         categoryAxis: {
-            categories: aCategories,
-            majorGridLines: {
-                visible: false
-            },
-            crosshair: {
-                visible: true
-            },
-            labels: {
-                rotation: "auto"
-            }
+            baseUnit: sBaseUnits
         },
         tooltip: {
             visible: true,  
             format: "{0}%",
-            template: " #= category#-20: <br/> #= value #  #= series.name #"
+            template: "#= value #  <br/>New #= series.field #"
         }
     });
-    
 }// end createChart()
+
 
 /**
  * Gets the charts data.
@@ -107,6 +107,11 @@ function downLoadData() {
             // console.log(data.USA.data[i].new_deaths);
             if(data.USA.data[i].new_deaths > 0){
                 chartInfo.dates.push( data.USA.data[i].date.substring(5) );
+                stats.push({
+                    date: new Date(data.USA.data[i].date),
+                    cases: data.USA.data[i].new_cases ,
+                    deaths: data.USA.data[i].new_deaths
+                });
                 chartInfo.newDeaths.push( data.USA.data[i].new_deaths );
                 chartInfo.newCases.push( data.USA.data[i].new_cases );
             }
@@ -114,21 +119,20 @@ function downLoadData() {
             total_deaths += data.USA.data[i].new_deaths;
             total_cases += data.USA.data[i].new_cases;
         }// end for data length
-        // console.log(total_cases);
         // set the value
         chartInfo.total_deaths = total_deaths;
         //console.log(chartInfo.total_deaths);
-        chartInfo.total_cases = total_cases
+        chartInfo.total_cases = total_cases;
         // display the charts
-        createChart("cases-chart", "Total Cases [" + chartInfo.total_cases + "]", "area", 
-        "Cases", "#86b1f7", chartInfo.newCases, chartInfo.dates); 
-        createChart("deaths-chart", "Total Deaths [" + chartInfo.total_deaths + "]", "line", 
-        "Deaths", "#2774f2", chartInfo.newDeaths, chartInfo.dates); 
+        createChart("cases-chart", stats, "cases", "months","#86b1f7", 
+        "Total Cases [" + chartInfo.total_cases + "]", "area");
+        createChart("deaths-chart", stats, "deaths", "months","#2774f2", 
+        "Total Deaths [" + chartInfo.total_deaths + "]", "line");
         let nCasesPercent = (total_cases / 3282000000 * 100).toFixed(2);
         let nCasesDeaths = (total_deaths / 3282000000 * 100).toFixed(4);
-        // display gauges
-        createGauge("cases", nCasesPercent);
-        createGauge("deaths", nCasesDeaths);
+        // display totals
+        displayTotalStats("cases", nCasesPercent);
+        displayTotalStats("deaths", nCasesDeaths);
         // remove loading indicator
         kendo.ui.progress($(".chart-loading"), false);
     }// end organizeData()
@@ -145,3 +149,8 @@ $( document ).ready(function() {
     kendo.ui.progress($(".chart-loading"), true);
     downLoadData();
 });
+
+// $("#chart-area").bind("kendo:skinChange", createChart);
+$(".box").bind("change", refresh);
+
+//chart-area
