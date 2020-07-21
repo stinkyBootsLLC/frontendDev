9b1567ba68934096fab8be4e7d095c9c317c35c9
stinkyBootsLLC <stinkybootsllc@gmail.com>
2020-07-21 19:13:33 -0400

    added File upload feature
    admin tool can now add a picture

--- recipes/process.php
+++ recipes/process.php
@@ -1,4 +1,66 @@
+
 <?php
+
+$target_dir = "recipeImages/";
+$target_file = $target_dir . basename($_FILES["picture"]["name"]);
+$imageName = basename($_FILES["picture"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["picture"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["picture"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["picture"]["tmp_name"], $target_file)) {
+    echo "The file ". basename( $_FILES["picture"]["name"]). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 ///// get all the post data
     $fields = array();
     $values = array();
@@ -7,6 +69,8 @@
         $values[] = $value;
     }// end foreach
     $post_results = array_combine($fields, $values);
+
+    print_r ($post_results);
 ///// end get all the post data
 
     $items = array();
@@ -43,6 +107,7 @@
             'id'             =>     $_POST['id'],  
             'title'          =>     $_POST["title"],  
             'description'    =>     $_POST["description"],
+            'picture'        =>     $imageName,
             'ingredients'    =>     $ingredients,
             'directions'     =>     $directions
         );  
@@ -56,3 +121,4 @@
         echo "JSON File not exits";  
     } // end if(file_exists()) 
 ?>
+
