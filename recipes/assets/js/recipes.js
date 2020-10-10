15fb9424ac89a1d312b649aff0113922e71d69a9
stinkyBootsLLC <stinkybootsllc@gmail.com>
2020-10-10 09:44:22 -0400

    accessibility fixed - 97% grade

--- recipes/assets/js/recipes.js
+++ recipes/assets/js/recipes.js
@@ -24,31 +24,36 @@ fetch(recipes)
       let recipeHeaderDiv = $("<div tabindex='0'></div>");
       recipeHeaderDiv.attr('class','card-header');
       recipeDiv.append(recipeHeaderDiv);
-      recipeHeaderDiv.append(`<i class='fas fa-utensils'></i>&nbsp;&nbsp;${data[recipe]["title"]}`);
+      recipeHeaderDiv.append(`<i class='fas fa-utensils' style='float: left;'></i>
+      <h1 style='font-size: 16pt; float: left; margin-left: 0.5em;'>${data[recipe]["title"]}</h1>`);
       let recipeBodyDiv = $("<div tabindex='0'></div>");
       recipeBodyDiv.attr('class','card-body');
       recipeDiv.append(recipeBodyDiv);
       recipeBodyDiv.append( `<img class='card-img-top' src='recipeImages/${data[recipe]["picture"]}' 
                                 alt='picture of ${data[recipe]["title"]}' style='height: 500px;'> ` );
-      recipeBodyDiv.append(`<p class='card-text'>Category: ${data[recipe]["Category"]}</p>`);
-      recipeBodyDiv.append(`<p class='card-text'>${data[recipe]["description"]}</p>`);
+      recipeBodyDiv.append(`<p class='card-text' tabindex='0'>Category: ${data[recipe]["Category"]}</p>`);
+      recipeBodyDiv.append(`<p class='card-text' tabindex='0'>${data[recipe]["description"]}</p>`);
       // create variables 
       let ingredients = data[recipe]["ingredients"];
       let instructions = data[recipe]["directions"];
       // add to div
-      recipeBodyDiv.append("<p>Ingredients:<p>");
+      recipeBodyDiv.append("<p tabindex='0'>Ingredients:<p>");
+      let ingrListDiv = $("<ul id='ingr_list'></ul>");
+      let stepListDiv = $("<ul id='step_list'></ul>");
+      recipeBodyDiv.append(ingrListDiv);
       // loop thru the ingredients
       for(item in ingredients){
         // create the list of ingredients
-        recipeBodyDiv.append(`<li>${ingredients[item]["name"]} - ${ingredients[item]["amount"]} 
+        ingrListDiv.append(`<li tabindex='0'>${ingredients[item]["name"]} - ${ingredients[item]["amount"]} 
                               ${ingredients[item]["unit"]}</li>`);
       }// end for(item in ingredients)
       // add header
-      recipeBodyDiv.append("<br><p>Directions:<p>");
+      recipeBodyDiv.append("<br><p tabindex='0'>Directions:<p>");
+      recipeBodyDiv.append(stepListDiv);
       // loop thru the instructions
       for(step in instructions){
         // create the list of instructions
-        recipeBodyDiv.append(`<li>${instructions[step]["Step"]}</li>`) ;
+        stepListDiv.append(`<li tabindex='0'>${instructions[step]["Step"]}</li>`) ;
       }// end for(step in instructions)
       // add as a child to the main div
       mainDivElement.append(recipeDiv);
